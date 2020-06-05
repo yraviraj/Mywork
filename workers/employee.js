d84f236b9e242fb7a79129c560626f0f9041436a
chaitanyasanapathi <chaitanya.sanapathi@gmail.com>
2020-06-05 23:04:25 +0530

    checkedin

--- workers/employee.js
+++ workers/employee.js
@@ -41,18 +41,14 @@ console.log(employees);
 */
 
 class EmployeeServices{
-
     constructor()
     {
         this.empcollection = [];
     }
-
     add(emp)
     {
         this.empcollection.push(emp);
     }
-
-
 }
 
 const employeeServices = new EmployeeServices();
@@ -62,16 +58,21 @@ for (let i = 1; i <= 100; i++) {
 
    // employees[i] = new Employee((100 + i), ('smith' + i), (10 + i)); // saving 100 Employee objects into employees object with 101 to 200 as key or properties
 }
-console.log(employeeServices.empcollection);
+// adding new properties such as pincode, state etc..
+let c=0;
+employeeServices.empcollection.forEach(obj=> {
+    obj["state"] = "state"+c;
+    obj.pincode = 50000 + c;
+    c++;
+});
 
+console.log(employeeServices.empcollection);
 
 const empKey = Object.keys(employeeServices.empcollection[1]);
 const empVal = Object.values(employeeServices.empcollection);
 
-console.log(empKey);
-console.log(empVal);
-
-
+console.log("empKey:"+empKey);
+console.log("emp values: " +empVal);
 
 function generateTableHead(table, empKey) {             // function for generating table head
     let thead = table.createTHead();
@@ -84,21 +85,48 @@ function generateTableHead(table, empKey) {             // function for generati
     let select = document.createTextNode("select");
     th.appendChild(select);
     row.appendChild(th);
-
+  
     for (const key of empKey) {
+        if((key == "id") || (key == "name") || (key == "age")) // to display 3 prop. id name and age
+        {
         let th = document.createElement("th");
         let text = document.createTextNode(key);
         th.appendChild(text);
         row.appendChild(th);
+        }
+        else
+        continue;
     }
 }
 let table = document.querySelector('table'); // getting table object form html table tag
+
+function empDetails(eid){
+empVal.forEach(emp => {
+    if(emp.id == eid)
+    {
+        document.getElementById("emp-details").style.visibility="visible";
+        document.getElementById("update-name").innerHTML= "Hi "+emp.name;
+        document.getElementById("emp-id").value= emp.id;
+        document.getElementById("emp-name").value = emp.name;
+        document.getElementById("emp-age").value = emp.age;        
+        document.getElementById("emp-state").value = emp.state;        
+        document.getElementById("emp-pincode").value = emp.pincode;        
+    } 
+});
+};
+let updateName = document.getElementById("emp-name");  //dynamically updating.
+updateName.onkeydown = updateNameDisplay;    //https://stackoverflow.com/questions/4790946/dynamically-displaying-input-values-using-javascript
+updateName.onkeyup = updateNameDisplay;
+
+function updateNameDisplay(){
+    document.getElementById("update-name").innerHTML = "Hi "+ this.value;
+}
+
 
 
 function generateTable(table, emplVal) {                 //function for generating table body
     for (let list of emplVal) {
         let row = table.insertRow();
-
 
         let ccell = row.insertCell();
         let x = document.createElement("INPUT");
@@ -107,6 +135,8 @@ function generateTable(table, emplVal) {                 //function for generati
         ccell.appendChild(x);
         let count = 1;
         for (let sublist in list) { 
+            if((sublist == "id") || (sublist == "name") || (sublist == "age"))    // to display 3 prop. id name and age
+            {
             if(count == 1){
                 let cell = row.insertCell();
                 let x = document.createElement("a");                // Create anchor element.
@@ -114,6 +144,7 @@ function generateTable(table, emplVal) {                 //function for generati
                 x.appendChild(text);                                // Append the text node to anchor element.
                 x.title = list[sublist];                            // Set the title.  which is optional
                 x.setAttribute("href", "#");                        // Set the href property.
+                x.setAttribute("onclick", "empDetails("+list[sublist]+")");
                 cell.appendChild(x);                                //appending the anchor element to the cell.    https://www.geeksforgeeks.org/how-to-create-a-link-in-javascript/
             }                                                       // https://www.kirupa.com/html5/handling_events_for_many_elements.htm
             else{
@@ -123,12 +154,13 @@ function generateTable(table, emplVal) {                 //function for generati
             }
             count++;
         }
+        else
+        continue;       
+        }
     }
 }
 generateTable(table, empVal);
 generateTableHead(table, empKey);
-
-
 
 let add = document.getElementById("add-btn");
 add.addEventListener("click",  () => {
@@ -156,8 +188,13 @@ addemp.addEventListener('click', () => {
     document.getElementById("abc").style.visibility = 'hidden';
 });
 
-
 function isChecked(checkBoxes) {
+   /* checkBoxes.forEach(element => {
+        if(element.checked)
+        {
+            return true;
+        }   
+    });  */
     for (i = 0; i < checkBoxes.length; i++) {
         if (checkBoxes[i].checked) {
             return true;
@@ -165,11 +202,17 @@ function isChecked(checkBoxes) {
     }
 }
 
-
 let delemp = document.getElementById("del-btn");
 delemp.addEventListener("click", () => {
     let checkboxes = document.getElementsByName("check-box");
     while (isChecked(checkboxes)) {
+        /*checkboxes.forEach((element, index) => {
+            if(element.checked)
+            {
+                table.deleteRow(index + 1);
+                break;
+            }          
+        });*/
         for (let i = 0; i < checkboxes.length; i++) {
             if (checkboxes[i].checked) {
                 table.deleteRow(i + 1);
@@ -178,6 +221,7 @@ delemp.addEventListener("click", () => {
         }
     }
 });
+
 
 
 
